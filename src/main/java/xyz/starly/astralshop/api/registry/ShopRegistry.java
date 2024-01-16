cd024b901ad530ac0735c7f220b23a6d46ac0c5a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-16 21:12:53 +0900

    feat: fix createShop function

--- src/main/java/xyz/starly/astralshop/api/registry/ShopRegistry.java
+++ src/main/java/xyz/starly/astralshop/api/registry/ShopRegistry.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ShopRegistry {
 
-    boolean createShop(String name, int rows);
+    boolean createShop(String name, String guiTitle);
 
     boolean deleteShop(String name);
 
