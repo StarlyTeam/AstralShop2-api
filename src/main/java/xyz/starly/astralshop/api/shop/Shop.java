138aa7690d293adb5af18e865139ff3a1e4c5dcd
idkNicks <yangdaeyeong0808@gmail.com>
2024-01-20 08:06:53 +0900

    feat: add @NotNull annotation & remove sound options

--- src/main/java/xyz/starly/astralshop/api/shop/Shop.java
+++ src/main/java/xyz/starly/astralshop/api/shop/Shop.java
@@ -1,12 +1,14 @@
 package xyz.starly.astralshop.api.shop;
 
-import xyz.starly.astralshop.api.sound.SoundType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public interface Shop {
 
-    String getName();
+    @NotNull String getName();
 
-    void setName(String name);
+    void setName(@NotNull String name);
 
     String getGuiTitle();
 
@@ -16,7 +18,5 @@ public interface Shop {
 
     void setNpc(String npc);
 
-    String getSound(SoundType soundType);
-
-    void setSound(SoundType soundType, String name);
+    List<ShopPage> getShopPages();
 }
